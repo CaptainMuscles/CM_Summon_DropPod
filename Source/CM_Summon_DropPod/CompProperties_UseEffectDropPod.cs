89b3cd4f90b19ad1988bb5e79d8e17504fad30fc
NathanHarbour <nathanharbour@gmail.com>
2021-01-08 17:02:24 -0600

    Initial commit

---
+++ Source/CM_Summon_DropPod/CompProperties_UseEffectDropPod.cs
@@ -0,0 +1,14 @@
+﻿using RimWorld;
+using Verse;
+namespace CM_Summon_DropPod
+{
+    public class CompProperties_UseEffectDropPod : CompProperties_Usable
+    {
+        public ThingDef thingDef;
+
+        public CompProperties_UseEffectDropPod()
+        {
+            compClass = typeof(CompProperties_UseEffectDropPod);
+        }
+    }
+}
---
+++ Source/CM_Summon_DropPod/CompUseEffect_DropPod.cs
@@ -0,0 +1,22 @@
+﻿using RimWorld;
+using Verse;
+
+namespace CM_Summon_DropPod
+{
+    public class CompUseEffect_DropPod : CompUseEffect
+    {
+        public CompProperties_UseEffectDropPod Props => (CompProperties_UseEffectDropPod)props;
+
+        public override void DoEffect(Pawn user)
+        {
+            if (Props.thingDef == null)
+                return;
+
+            IntVec3 dropSpot;
+            Thing thing = ThingMaker.MakeThing(Props.thingDef);
+
+            if (thing != null && DropCellFinder.TryFindDropSpotNear(parent.PositionHeld, parent.MapHeld, out dropSpot, false, false, false))
+                TradeUtility.SpawnDropPod(dropSpot, parent.MapHeld, thing);
+        }
+    }
+}
